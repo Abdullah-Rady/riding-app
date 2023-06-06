7e4820fe2d3443e8fb9217919e867d75b7ef60ba
Abdullah Rady <abdosam@live.co.uk>
2023-06-06 17:08:05 +0300

    f

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sshcmd="ssh -t abdo@abdosam.duckdns.org"
-$sshcmd screen -S "deployment" /home/abdo/app/riding-app/prod_deploy.sh
+sshcmd="ssh -t  abdo@abdosam.duckdns.org"
+$sshcmd -S "deployment" /home/abdo/app/riding-app/prod_deploy.sh
+#$sshcmd -S "deployment" /home/abdo/a.sh
